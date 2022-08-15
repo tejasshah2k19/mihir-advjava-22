48024d1e2eb232de9e797787ca0e3fd4a4ebf3a6
tejas <tejasshah@gmail.com>
2022-08-15 13:24:25 +0530

    context

--- WebContent/AdminLogin.jsp
+++ WebContent/AdminLogin.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,9 +8,10 @@
 </head>
 <body>
 	<form action="AdminLoginServlet">
-		Email : <input type="text" name="email"/><br><br>
-		Password : <input type="password" name="password"/><br><br>
-		<input type="submit" value="Login"/>
+		Email : <input type="text" name="email" /><br>
+		<br> Password : <input type="password" name="password" /><br>
+		<br> <input type="submit" value="Login" />
 	</form>
+	<br> ${error}
 </body>
 </html>
--- src/com/controller/AdminLoginServlet.java
+++ src/com/controller/AdminLoginServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,18 +15,24 @@ public class AdminLoginServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		ServletContext context = getServletContext();
 		ServletConfig config = getServletConfig();
 
 		String adminEmail = config.getInitParameter("email");
 		String adminPassword = config.getInitParameter("password");
 
+		String gEmail = context.getInitParameter("gemail");
+		String gPassword = context.getInitParameter("gpassword");
+
 		String email = request.getParameter("email");
 		String password = request.getParameter("password");
 
 		RequestDispatcher rd = null;
-		if (email.equals(adminEmail) && password.equals(adminPassword)) {
+		if ((email.equals(adminEmail) && password.equals(adminPassword))
+				|| (email.equals(gEmail) && password.equals(gPassword))) {
 			rd = request.getRequestDispatcher("AdminHome.jsp");
 		} else {
+			request.setAttribute("error", "Invalid Credentials");
 			rd = request.getRequestDispatcher("AdminLogin.jsp");
 		}
 		rd.forward(request, response);

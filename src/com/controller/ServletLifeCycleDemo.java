a8034348ca58e63b4c4091a3846e37e912c8894a
tejas <tejasshah@gmail.com>
2022-08-20 11:52:59 +0530

    servlet lifecycle

---
+++ src/com/controller/ServletLifeCycleDemo.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletLifeCycleDemo")
+public class ServletLifeCycleDemo extends HttpServlet {
+	public void init() throws ServletException {
+		// one
+		System.out.println("init()"); // allocation
+	}
+
+	// n time -- request
+	// logic
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		System.out.println("service()");
+	}
+
+	// one
+	public void destroy() {
+		System.out.println("destroy()");// free
+	}
+
+}
